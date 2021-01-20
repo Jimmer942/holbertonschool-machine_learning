71b7d41cc74a8a6ac0b221871446d109f0f8865c
Jimmer942 <942@holbertonschool.com>
2021-01-20 15:25:09 -0500

    10-ill_use_my_scale.py

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Function that calculates the shape of a numpy.ndarray"""
+
+
+def np_shape(matrix):
+    """
+    INPUT: A matrix
+    OUTPUT: the matrix's shape
+    """
+    return matrix.shape
