2a80c586d1f528b58dafb7e5e731b68350ffb55f
Jimmer942 <942@holbertonschool.com>
2021-01-20 14:42:35 -0500

    5-across_the_planes.py

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
+"""Function that adds two matrices element-wise"""
+
 
 def add_matrices2D(mat1, mat2):
+    """
+    INPUT: two matrix
+    OUTPUT: A matrix containing the sum position by position
+    """
     len(mat1[0])
     len(mat1)
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
