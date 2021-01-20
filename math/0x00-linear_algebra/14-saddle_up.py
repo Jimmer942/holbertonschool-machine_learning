4e019541590597ee2450144c2a8cbebb4a1b24cb
Jimmer942 <942@holbertonschool.com>
2021-01-20 15:45:51 -0500

    14-saddle_up.py

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Function def np_matmul(mat1, mat2): that performs matrix multiplication
+"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    INPUT: Two matrix
+    OUTPUT: the matrix multiplication
+    """
+    return np.matmul(mat1, mat2)
