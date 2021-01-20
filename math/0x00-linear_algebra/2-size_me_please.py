70e75c2a5a3628c276eb64a3e0dcb73f76a1ebf4
Jimmer942 <942@holbertonschool.com>
2021-01-20 14:35:10 -0500

    2-size_me_please.py

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 """function def matrix_shape(matrix): that calculates the shape of a matrix:"""
 
+
 def matrix_shape(matrix):
+    """INPUT: a matrix that wans to know the shape
+       OUTPUT: the shape of the given matrix
+    """
     m = list()
     arr = matrix[0]
     m.append(len(matrix))
