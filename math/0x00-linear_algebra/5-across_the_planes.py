77b0abb63c525e7137b6686a237ff411ece3c312
Jimmer942 <942@holbertonschool.com>
2021-01-20 14:50:06 -0500

    5-across_the_planes.py

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -7,14 +7,12 @@ def add_matrices2D(mat1, mat2):
     INPUT: two matrix
     OUTPUT: A matrix containing the sum position by position
     """
-    if len(mat1) != len(mat2):
-        return None
-    if len(mat1[0]) != len(mat2[0]):
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
     new_ma = list()
     ar = list()
     for i in range(len(mat1)):
-        for j in range(len(mat2)):
+        for j in range(len(mat2[0])):
             ar.append(mat1[i][j] + mat2[i][j])
         new_ma.append(ar)
         ar = []
