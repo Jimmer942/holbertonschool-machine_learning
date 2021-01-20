840bc45fd0fe2a59a863ca72c3be6506ae007bae
Jimmer942 <942@holbertonschool.com>
2021-01-20 15:27:27 -0500

    11-the_western_exchange.py

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Function def np_transpose(matrix): that transposes matrix"""
+
+
+def np_transpose(matrix):
+    """
+    INPUT: A matrix
+    OUTPUT: the matrix's transpose
+    """
+    return matrix.transpose()
