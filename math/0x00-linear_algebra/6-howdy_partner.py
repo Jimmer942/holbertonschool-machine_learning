73aa835ae9adf961e594d1fe9a9849d83a41c7eb
Jimmer942 <942@holbertonschool.com>
2021-01-20 14:43:57 -0500

    6-howdy_partner.py

--- math/0x00-linear_algebra/6-howdy_partner.py
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+"""Function def cat_arrays(arr1, arr2): that concatenates two arrays"""
+
 
 def cat_arrays(arr1, arr2):
+    """
+    INPUT: two arrays
+    OUTPUT: An array containind both of the input elements
+    """
     return arr1 + arr2
