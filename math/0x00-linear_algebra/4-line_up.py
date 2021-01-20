fcd53fe4737d9bb0f39218b4216cd8be3414cec1
Jimmer942 <942@holbertonschool.com>
2021-01-20 14:40:53 -0500

    4-line_up.py

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
+"""Function def add_arrays(arr1, arr2): that adds two arrays element-wise"""
+
 
 def add_arrays(arr1, arr2):
+    """
+    INPUT: two arrays
+    OUTPUT: An array containing the sum position by position
+    """
     if len(arr1) != len(arr2):
         return None
     sum_arr = list()
