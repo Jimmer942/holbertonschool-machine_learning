45e008dcb8dec1e85d8e4b2c1264c2ecdc028fed
Jimmer942 <942@holbertonschool.com>
2021-01-28 09:37:10 -0500

    17-integrate.py

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" INtegration """
+
+
+def poly_integral(poly):
+    """
+    INPUT: polinom
+    OUTPUT integration polinom
+    """
+    if poly == [] or type(poly) is not list:
+        return None
+
+    if type(poly[0]) is not int and type(poly[0]) is not float:
+        return None
+
+    inte = list()
+    inte.append(0)
+
+    if len(poly) == 1:
+        return [C]
+
+    print(poly)
+    for n in range(len(poly)):
+        aux = poly[n] / (n + 1)
+        if aux % 1 == 0:
+            inte.append(int(aux))
+        else:
+            inte.append(aux)
+    return inte
