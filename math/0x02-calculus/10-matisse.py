50278c4b7ec72da1a8abbc9d4da0ed37d92ac476
Jimmer942 <942@holbertonschool.com>
2021-01-27 19:36:40 -0500

    10-matisse.py

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Derivation """
+
+
+def poly_derivative(poly):
+    """
+    INPUT: polinom
+    OUTPUT d(polinom)/dx
+    """
+    if poly == [] or type(poly) is not list:
+        return None
+    if type(poly[0]) is not int and type(poly[0]) is not float:
+        return None
+
+    der = list()
+
+    if len(poly) == 1:
+        return [0]
+
+    for n in range(1, len(poly)):
+        print(poly[n], n, n-1)
+        der.append(poly[n] * n)
+    return der
