7b77c0a8e7e577457d7e4f05329bf643b182265a
Jimmer942 <942@holbertonschool.com>
2021-01-28 21:42:44 -0500

    17-integrate.py

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -12,7 +12,7 @@ def poly_integral(poly, C=0):
     if type(poly[0]) is not int and type(poly[0]) is not float:
         return None
     if len(poly) == 1:
-        return [C, poly[0]]
+        return [C]
 
     inte = list()
     inte.append(C)
