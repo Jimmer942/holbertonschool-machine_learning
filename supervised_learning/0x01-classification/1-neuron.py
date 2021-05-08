e95e79e419ad7368a8ef88a193eceafed9023f48
Jimmer942 <942@holbertonschool.com>
2021-05-07 20:41:46 -0500

    Private neuron

---
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+class Neuron():
+    def __init__(self, nx):
+
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+
+        self.nx = nx
+        __self.W = np.random.randn(nx).reshape(1, nx)
+        __self.b = 0
+        __self.A = 0
